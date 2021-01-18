37a4637052578b916d83712620636764ed32ab98
Changyeon-Seo <scy7351@naver.com>
2021-01-18 19:35:26 +0900

    2021.01.18

--- psroadmap/bruteforce/3085.py
+++ psroadmap/bruteforce/3085.py
@@ -1,29 +1,58 @@
 import sys
-from itertools import groupby
+
+
+def check_continuous_char(input_array):
+    res = 1
+    current_count = 1
+    current_char = input_array[0]
+
+    for char in input_array[1:]:
+        if current_char == char:
+            current_count += 1
+        else:
+            if res < current_count:
+                res = current_count
+            current_char = char
+            current_count = 1
+    if res < current_count:
+        res = current_count
+    return res
+
+
+def flatten_2d_array(input_array):
+    flatten = []
+    for row in input_array:
+        flatten.append(row)
+    for i in range(len(input_array)):
+        line = []
+        for j in range(len(input_array)):
+            line.append(input_array[j][i])
+        flatten.append(line)
+    return flatten
+
+
+def get_max_length(input_board):
+    res_arr = []
+    for array in flatten_2d_array(input_board):
+        res_arr.append(check_continuous_char(array))
+    return max(res_arr)
+
 
 input = sys.stdin.readline
-
-
-def max_continuous_length(array):
-    return max((sum(1 for _ in g) for k, g in groupby(data)), default=0)
-
-
-def get_longest_strings(rows):
-    for i in range(count):
-        column = []
-        for j in range(count):
-            column.append(rows[j][i])
-        rows.append(column)
-
-
-data = []
-count = int(input().rstrip())
+count = int(input())
+board = [list(input().rstrip()) for i in range(count)]
+result = []
 
 for i in range(count):
-    candy_str = input().rstrip()
-    candy_arr = []
-    for candy in candy_str:
-        candy_arr.append(candy)
-    data.append(candy_arr)
+    for j in range(count - 1):
+        board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+        result.append(get_max_length(board))
+        board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
 
-get_longest_strings(data)
+for i in range(count):
+    for j in range(count - 1):
+        board[j][i], board[j + 1][i] = board[j + 1][i], board[j][i]
+        result.append(get_max_length(board))
+        board[j][i], board[j + 1][i] = board[j + 1][i], board[j][i]
+
+print(max(result))
